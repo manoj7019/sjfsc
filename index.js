504a64ae29cbc354b9ef111f322bc2c088ff7b07
Manoj A N <manojhustler@gmail.com>
2024-05-10 20:48:18 +0530

    add 4 Lead Sheets so we generate leads

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-import { GoogleAuthProvider } from "firebase/auth/cordova";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import { getAuth, googleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+function initGoogleAuth() {
+    gapi.load('auth2', function() {
+      gapi.auth2.init({
+        client_id: '57273358504-kl41t8su8i0n2tlc6t27bl9nbujc5lki.apps.googleusercontent.com'
+      });
+    });
+  }
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyCypdG-rqzfGFYBuxDQUNk8qD73wCUG5dA",
-    authDomain: "sjfsc-f72f7.firebaseapp.com",
-    databaseURL: "https://sjfsc-f72f7-default-rtdb.firebaseio.com",
-    projectId: "sjfsc-f72f7",
-    storageBucket: "sjfsc-f72f7.appspot.com",
-    messagingSenderId: "807759975556",
-    appId: "1:807759975556:web:675925b68bceb59e1c9ed1"
+  function signIn() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signIn().then(function(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId());
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    });
+  }
+
+  window.onload = function() {
+    initGoogleAuth();
   };
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  auth.languageCode = 'en';
-
-  const provider = new GoogleAuthProvider();
-
-  const googleLogin = document.getElementById('google-login-btn');
-  googleLogin.addEventListener('click', function() {
-    alert(5);
-  })
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function() {
+      console.log('User signed out.');
+    });
+  }
